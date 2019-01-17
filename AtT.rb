6f3f4ec834dd1a94e3e5d8590f5ee67000d9ddc0
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-01-17 15:17:28 -0600

    Function defines and initializes Left and right children node indexes

--- AtT.rb
+++ AtT.rb
@@ -6,5 +6,10 @@ def array_to_tree array, index=0
 	# Check if index is out of range
 	if index > array.length 
 		return
-	end	
+	end
+	
+	# Left and right children node indexes
+	left = (index * 2) + 1
+
+	right = left + 1	
 end	
